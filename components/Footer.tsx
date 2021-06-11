4b61fc67e26e09919db74ff40501f712f3e76215
Lucas Phang <lucasphang98@gmail.com>
2021-06-11 15:25:41 +1000

    Add skeleton header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,6 @@
 import React, { ReactNode } from 'react'
 
 
-type Props = {
-    children?: ReactNode
-    title?: string
-}
-
 const Footer = () => (
     <footer className="flex items-center justify-center bg-gray-200">
         < hr />
