adf01e4e398a1c5276d409a1c637013f9bb1a647
Lucas Phang <lucasphang98@gmail.com>
2021-06-25 15:06:30 +1000

    Update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,29 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 
-
+const headerLinks = [
+  {
+    to: '/faq',
+    title: 'FAQ'
+  }
+]
 
 
 const Header = () => (
-    <header className="flex items-center justify-center bg-gray-200">
-        <nav>
-            <Link href="/">
-                <a>Home</a>
-            </Link>{' '}
-            |{' '}
-            <Link href="/about">
-                <a>About</a>
-            </Link>{' '}
-            |{' '}
-            <Link href="/users">
-                <a>Users List</a>
-            </Link>{' '}
-            | <a href="/api/users">Users API</a>
-        </nav>
-    </header >
+  <header className="flex justify-between bg-gray-200 p-7">
+    <img src='lokinet-logo-black.png' className='max-h-10' />
+    <div className='flex items-center justify-end flex-grow pl-5 pr-5'>
+      <nav>
+        <Link href='/faq'>
+          FAQ
+        </Link>
+      </nav>
+    </div>
+    <div className='flex items-center'>
+      <img src='light-switch.png' className='max-h-10' />
+    </div>
+
+  </header >
 )
 
 export default Header;
